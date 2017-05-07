3be8fac7b5090ee80b896a76ce02d8a9e3a84238
Anton Alekseev <Anton Alekseev>
2017-05-07 10:30:28 +0300

    Done API

--- index.js
+++ index.js
@@ -1,20 +1,57 @@
 var express = require('express');
 var app = express();
+var mongodb = require("mongodb");
+var db;
+var ObjectID = require('mongodb').ObjectID;
+var url = "mongodb://heroku_spf2nvhx:c3pgsoa5k960kebae90fna50ft@ds133231.mlab.com:33231/heroku_spf2nvhx";
+
+mongodb.MongoClient.connect(process.env.MONGODB_URI||url, function (err, database)
+{
+    if (err)
+    {
+        console.log(err);
+        process.exit(1);
+    }
+
+    db = database;
+
+});
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+  response.send('All right');
 });
 
-app.get('/kek', function(request, response) {
-    response.send('AllRight');
+app.get('/med/:id', function(request, response) {
+    var collection = db.collection('AllDesc');
+    var id = ObjectID.createFromHexString(request.params.id);
+    collection.find({_id: id}).toArray(function(err, docs) {
+        console.log(request.params["id"]);
+        console.dir(docs);
+        response.send(docs);
+    });
+});
+
+app.get('/alldesc', function(request, response) {
+    var collection = db.collection('AllDesc');
+    collection.find({}).toArray(function(err, docs) {
+        console.log("Found the following records");
+        console.dir(docs);
+        response.send(docs);
+    });
+
+});
+
+app.get('/allmeds', function(request, response) {
+    var collection = db.collection('AllMeds');
+    collection.find({}).toArray(function(err, docs) {
+        console.log("Found the following records");
+        console.dir(docs);
+        response.send(docs);
+    });
+
 });
 
 app.listen(app.get('port'), function() {
